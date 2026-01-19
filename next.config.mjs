5b397f556ecf954ade2b4086dbc4283c186fc3e5
Space <ndoemo02@gmail.com>
2026-01-19 09:41:15 +0100

    fix(build): remove conflicting Cesium Webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import CopyWebpackPlugin from 'copy-webpack-plugin';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -7,25 +6,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    webpack: (config, { webpack, isServer }) => {
-        if (!isServer) {
-            config.plugins.push(
-                new CopyWebpackPlugin({
-                    patterns: [
-                        {
-                            from: path.join(__dirname, 'node_modules/cesium/Build/Cesium'),
-                            to: 'cesium',
-                        },
-                    ],
-                })
-            );
-        }
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            cesium: path.resolve(__dirname, 'node_modules/cesium'),
-        };
-        return config;
-    },
 };
 
 export default nextConfig;
